daddccad6aaa2835f942101860e39e2947f22d60
cherislive <cherislive@163.com>
2016-08-17 18:23:25 +0800

    DELETE FILES

--- app/views/_signup.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<form class="mod-form">
-  <div class="form-item"><input type="text" placeholder="请输入邮箱"></div>
-  <div class="form-item"><input type="text" placeholder="您的真实姓名"></div>
-  <div class="form-item"><input type="password" placeholder="密码总得填的"></div>
-  <div class="form-item"><input type="button" value="注册"></div>
-  <p>确认遵守用户协议  <a href="#" v-link="{path:'/'}">返回首页</a></p>
-</form>
-</template>
